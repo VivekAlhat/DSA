7fa3583528097a36babc0fabfb62c279436a0992
VivekAlhat <vivekalhat14@outlook.com>
2020-08-27 14:45:35 +0530

    Create Passing Arrays

--- CPP-Concepts/Functions/Passing-arrays.cpp
+++ CPP-Concepts/Functions/Passing-arrays.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void passValue(int a[], int n)
+{
+    // In this function a is just a pointer to array
+    a[0] = 10;
+}
+
 int *returnArr(int a[])
 {
     return a;
@@ -20,6 +26,7 @@ int main()
     int arr[] = {1, 2, 3, 4, 5};
     int *tmp;
     tmp = returnArr(arr);
+    passValue(tmp, 5);
     display(tmp, 5);
     return 0;
 }
