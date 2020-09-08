2c2e90351635c4009f6d0eb8f49165a67fbc85d5
VivekAlhat <vivekalhat14@outlook.com>
2020-09-08 23:01:08 +0530

    String Duplicates Bitwise Method

---
+++ Data Structures/String/StringDuplicatesBitwise.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string s;
+	cin>>s;
+	long int N=0, X=0;
+	for(int i=0;s[i];++i){
+		X=1;
+		X = X<<(s[i]-'a');
+
+		if((X & N) > 0){
+			cout<<s[i]<<endl;
+		}else{
+			N = X | N;
+		}
+	}
+	return 0;
+}
