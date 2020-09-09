38bbbd4f1991e7afdd667f83c47c976e3a51574a
VivekAlhat <vivekalhat14@outlook.com>
2020-09-09 23:27:35 +0530

    Permutations of a string using swapping approach

---
+++ Data Structures/String/PermutationsSwap.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void perm(string s, int l, int h){
+	if(l==h){
+		cout<<s<<endl;
+	}else{
+		for(int i=l;i<=h;++i){
+			swap(s[l],s[i]);
+			perm(s,l+1,h);
+			swap(s[l],s[i]);
+		}
+	}
+}
+
+int main(){
+	int l,h;
+	string s;
+	cin>>s;
+	l = 0;
+	h = s.size()-1;
+	perm(s, l , h);
+	return 0;
+}
